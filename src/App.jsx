f5112fb3c8e1f76c52f1d94200cbd0662d6da0bf
bangina <bbangina@gmail.com>
2023-06-12 13:51:54 +0900

    add Header& set module federation

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import "./index.css";
+import Header from "./Header";
 
 const App = () => (
-  <div>
-    - App name : MFE-HEADER
-    <br />- Version : 1.0.0
-    <br />- Webpack 5
+  <div
+    style={{
+      border: "1px solid #ccc",
+      padding: "10px",
+      margin: "10px",
+      width: "300px",
+    }}
+  >
+    I'm MFE-HEADER app 😃
+    <Header />
   </div>
 );
 
---
+++ src/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div
+      style={{
+        backgroundColor: "lightblue",
+        padding: "10px",
+        margin: "10px",
+        border: "1px solid black",
+        textAlign: "center",
+      }}
+    >
+      Header
+    </div>
+  );
+}
